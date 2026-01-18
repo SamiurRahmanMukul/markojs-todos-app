22a6b96f2ba3086aae1824540a30159983144639
Samiur Rahman Mukul <sr.mukul9090@gmail.com>
2026-01-18 09:54:44 +0600

    Refactor: reorder imports alphabetically in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import marko from "@marko/run/vite";
 import staticAdapter from "@marko/run-adapter-static";
+import marko from "@marko/run/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   base: process.env.NODE_ENV === "production"
